019a8131c182fbd8ec75111ceaf668d151272283
Jan Steikowski <j.steikowski@aol.com>
2024-10-06 20:14:47 +0700

    Add html template for dashboard

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+  "fmt"
   "net/http"
+  "html/template"
 )
 
 
 func (app *application) getDashboard(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Hello World!"))
+  tmpl, err := template.ParseFiles("./ui/html/pages/dashboard.tmpl.html")
+  if err != nil {
+    fmt.Println(err)
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    return
+  }
+
+  err = tmpl.Execute(w, nil)
+  if err != nil {
+    fmt.Println(err)
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+  }
 }
 
 func (app *application) getBooks(w http.ResponseWriter, r *http.Request) {
