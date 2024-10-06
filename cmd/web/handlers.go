1b04c0feb7a570767a0dcaa13a7d45fef3d45e96
Jan Steikowski <j.steikowski@aol.com>
2024-10-06 18:44:35 +0700

    Add handler functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+  "net/http"
+)
+
+
+func (app *application) getDashboard(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Hello World!"))
+}
+
+func (app *application) getBooks(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Here are the books."))
+}
+
+func (app *application) getBooksCreate(w http.ResponseWriter, r *http.Request) {
+  return
+}
+
+func (app *application) postBooksCreate(w http.ResponseWriter, r *http.Request) {
+  return
+}
+
+func (app *application) getBookView(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Here comes your book."))
+}
+
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,21 +7,12 @@ import (
 func (app *application) routes() http.Handler {
   mux := http.NewServeMux()
 
-  mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-      w.Write([]byte("Hello World!"))
-  })
+  mux.HandleFunc("/", app.getDashboard)
 
-  mux.HandleFunc("GET /books", func (w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Here are the books."))
-  })
-
-  mux.HandleFunc("POST /books", func (w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("New books here."))
-  })
-
-  mux.HandleFunc("GET /books/{id}", func (w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Here comes your book."))
-  })
+  mux.HandleFunc("GET /books", app.getBooks)
+  mux.HandleFunc("POST /books", app.postBooksCreate)
+  mux.HandleFunc("GET /books/create", app.getBooksCreate)
+  mux.HandleFunc("GET /books/{id}", app.getBookView)
 
   return mux
 }
