5f15d54b71ac3d81fe632cc05a227d836565b12e
Jan Steikowski <j.steikowski@aol.com>
2024-10-06 10:25:09 +0700

    Create basic server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+  "net/http"
+  "flag"
   "fmt"
+  "os"
 )
 
 func main () {
-  fmt.Println("Hello");
+  addr := flag.String("addr", ":4444", "HTTP network address.")
+  server := &http.Server{
+    Addr: *addr,
+    Handler: routes(),
+  }
+
+
+  fmt.Printf("Starting server at localhost%s\n", *addr)
+  err := server.ListenAndServe()
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+  "net/http"
+)
+
+func routes() http.Handler {
+  mux := http.NewServeMux()
+
+  mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+      w.Write([]byte("Hello World!"))
+  })
+  return mux
+}
