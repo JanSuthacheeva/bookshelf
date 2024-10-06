c8cc57567deffdec540a76b01ef862a6f9ce8609
Jan Steikowski <j.steikowski@aol.com>
2024-10-06 15:07:57 +0700

    Add book routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,5 +10,18 @@ func (app *application) routes() http.Handler {
   mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
       w.Write([]byte("Hello World!"))
   })
+
+  mux.HandleFunc("GET /books", func (w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Here are the books."))
+  })
+
+  mux.HandleFunc("POST /books", func (w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("New books here."))
+  })
+
+  mux.HandleFunc("GET /books/{id}", func (w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Here comes your book."))
+  })
+
   return mux
 }
