3187aa285d46481661658f42817a2072a73ba7d0
Jan Steikowski <j.steikowski@aol.com>
2024-10-06 20:43:35 +0700

    Add tailwind to project

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,10 +2,14 @@ package main
 
 import (
   "net/http"
+
+  "github.com/jansuthacheeva/bookshelf/ui"
 )
 
 func (app *application) routes() http.Handler {
   mux := http.NewServeMux()
+
+  mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
   mux.HandleFunc("/", app.getDashboard)
 
