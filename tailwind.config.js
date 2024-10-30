93dd8ed8ae25ed1ee0141bd0bfb05f3009fc4225
Jan Suthacheeva <j.steikowski@aol.com>
2024-10-30 20:16:43 +0700

    Add text-2xs to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,15 @@ module.exports = {
     "./ui/html/**/*.html",
   ],
   theme: {
+    fontSize: {
+      '2xs': '0.6rem',
+    },
     extend: {
       backgroundImage: {
         'orange-linear': 'linear-gradient(-7deg, #EC2C5A, #FA7C54)',
       },
       maxWidth: {
-        '3xl': '1800px'
+        '3xl': '1800px',
       },
       colors: {
         'gray-icon': '#8A8A8A'
