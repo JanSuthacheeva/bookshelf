4309c2df2fdca7405ed147e465bc467354b9c188
Jan Suthacheeva <j.steikowski@aol.com>
2024-11-04 07:05:19 +0700

    Add user model mocks

--- internal/models/mocks/users.go
+++ internal/models/mocks/users.go
@@ -1,3 +1,32 @@
 package mocks
 
-import ()
+import (
+	"github.com/jansuthacheeva/bookshelf/internal/models"
+)
+
+type UserModel struct{}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	switch email {
+	case "duplicate@example.com":
+		return models.ErrDuplicateEmail
+	default:
+		return nil
+	}
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	if email == "mock@example.com" && password == "mockPass" {
+		return 1, nil
+	}
+	return 0, models.ErrInvalidCredentials
+}
+
+func (m *UserModel) Exists(id int) (bool, error) {
+	switch id {
+	case 1:
+		return true, nil
+	default:
+		return false, nil
+	}
+}
