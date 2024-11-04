3b739a1b218404c24a058dfbab5347b28c62f4a6
Jan Suthacheeva <j.steikowski@aol.com>
2024-11-04 08:24:04 +0700

    Add books view test - need further development

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -7,6 +7,58 @@ import (
 	"github.com/jansuthacheeva/bookshelf/internal/assert"
 )
 
+func TestBookView(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Valid ID",
+			urlPath:  "/books/1",
+			wantCode: http.StatusOK,
+			wantBody: "A mock book",
+		},
+		{
+			name:     "Non-existent ID",
+			urlPath:  "/books/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Negative ID",
+			urlPath:  "/books/-2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Decimal  ID",
+			urlPath:  "/books/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "String ID",
+			urlPath:  "/books/foo",
+			wantCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+
+			assert.Equal(t, code, tt.wantCode)
+
+			if tt.wantBody != "" {
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
+}
+
 func TestPing(t *testing.T) {
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
